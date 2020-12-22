9fea06c201e92a0cf3b71efbfc669b5ac5cfce47
Nitikornns <36132908+Nitikornns@users.noreply.github.com>
2020-12-23 01:34:43 +0700

    commit

--- backend/students/views.py
+++ backend/students/views.py
@@ -2,8 +2,6 @@ from rest_framework.viewsets import ModelViewSet
 
 from .models import Student
 from .serializers import StudentSerializer
-# Create your views here.
-
 
 class StudentsViewSet(ModelViewSet):
     queryset = Student.objects.all()
